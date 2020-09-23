9c0ffea104d547e3a459ccd52b1fa16366bc3d85
Maxim Orlov <maximorl@gmail.com>
2020-09-23 13:17:50 +0200

    Testing deploy hook

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(async (req, res) => {
-  return res.end(`Hello world! v6`);
+  return res.end(`Hello world! v7`);
 });
 
 server.listen(PORT, () => {
